1770718e76da99d95acafd69908905877882a278
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-10-19 18:50:32 +0300

    task1 task2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImgCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCardMarkup(images) {
+  const markup = images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`;
+    })
+    .join('');
+  return markup;
+}
+
+const lightbox = new SimpleLightbox(`.gallery__link`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(
+  Number(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+);
+
+const onTimeUpdate = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
